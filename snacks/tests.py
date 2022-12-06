bf1322cd844735be35e32c319fb6d8b323f5aa9f
WalaaAtiah <walaaatiah97@gmail.com>
2022-12-06 06:41:32 +0000

    test:add test and style

--- snacks/tests.py
+++ snacks/tests.py
@@ -34,37 +34,41 @@ class test_snack(TestCase):
         response = self.client.get(url)
         self.assertTemplateUsed(response,'snack_detail.html')  
 
+    def test_create_view_length(self):
+    
+           data={
+               'title':'test',
+               'purchaser':self.user.id,
+               'description':'description',
+               'image':"image"
+    
+            }
+           url = reverse('Snack_Create')
+           response= self.client.post(path=url,data=data,follow=True)
+           self.assertEqual(len(Snack.objects.all()),2)
+         
+    def test_create_view_Template(self):
+    
+           data={
+               'title':'test',
+               'purchaser':self.user.id,
+               'description':'description',
+               'image':"image"
+    
+            }
+           url = reverse('Snack_Create')
+           response= self.client.post(path=url,data=data,follow=True)
+           self.assertTemplateUsed(response,'snack_detail.html')
+       
     
 
-    # def test_create_view_Redirects(self):
-    #     data={
-    #         "title"  : 'test2',
-    #         "purchaser" : self.user ,
-    #         "description" : "description2 ",
-    #         "image" :"image2"
-    #      }
-    #     url = reverse('Snack_Create')
-    #     response= self.client.post(path=url,data=data,follow=True)
-    #     self.assertRedirects(response,reverse('Snack_Detail',args=[2]))
-
-    # def test_create_view_length(self):
-    #     data={
-    #         "title"  : 'test2',
-    #         "purchaser" : self.user ,
-    #         "description" : "description2 ",
-    #         "image" :"image2"
-    #      }
-    #     url = reverse('Snack_Create')
-    #     response= self.client.post(path=url,data=data,follow=True)
-    #     self.assertEqual(len(Snack.objects.all()),2)
-       
-    # def test_create_view_Template(self):
-    #     data={
-    #         "title"  : 'test2',
-    #         "purchaser" : self.user ,
-    #         "description" : "description2 ",
-    #         "image" :"image2"
-    #      }
-    #     url = reverse('Snack_Create')
-    #     response= self.client.post(path=url,data=data,follow=True)   
-    #     self.assertTemplateUsed(response,'snack_detail.html')
+    def test_create_view_Redirects(self):
+        data={
+            "title"  : 'test2',
+            "purchaser" : self.user.id ,
+            "description" : "description2 ",
+            "image" :"image2"
+         }
+        url = reverse('Snack_Create')
+        response= self.client.post(path=url,data=data,follow=True)
+        self.assertRedirects(response,reverse('Snack_Detail',args=[2]))
